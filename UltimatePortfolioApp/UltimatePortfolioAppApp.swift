b1bcce2c369cda3a366c39762daaba46982c06fa
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-01-01 23:06:34 -0500

    Creates project

---
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioAppApp.swift
+//  UltimatePortfolioApp
+//
+//  Created by Alonso on 1/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
